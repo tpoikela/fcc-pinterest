5a82a8b6b842dd7c295fd6715e7e827d3d694b2b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-29 22:37:13 +0200

    Added redux-related code.

--- client/jsx/app.jsx
+++ client/jsx/app.jsx
@@ -1,16 +1,63 @@
 
 'use strict';
 
-const ProfileTop = require('./profile-top');
+import {profileReducer} from '../redux/reducers.js';
+import {getUserInfo, actionClicked,
+    fetchUser, receiveUser} from '../redux/actions';
 
-var profileTop = document.querySelector('#profile-app');
-var booksTop = null;
+import ThunkMiddleware from 'redux-thunk';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-if (profileTop) {
-    ReactDOM.render(<ProfileTop />, profileTop);
+const ReactRedux = require('react-redux');
+
+const ProfileTop = require('./profile-top');
+
+const Provider = ReactRedux.Provider;
+
+let profTopElem = document.querySelector('#profile-app');
+var booksTop = null;
+
+let store = createStore(
+    combineReducers({
+        profileReducer: profileReducer
+    }),
+    applyMiddleware(
+        ThunkMiddleware // Lets us dispatch functions
+    )
+);
+
+//---------------
+// MAP FUNCTIONS
+//---------------
+
+let mapDispatchToProps = dispatch => ({
+    onClickButton: () => dispatch(actionClicked()),
+    getUserInfo: () => dispatch(getUserInfo())
+});
+
+let mapStateToProps = (state) => {
+    console.log('mapStateToProps with ' + JSON.stringify(state));
+    return {
+        userData: state.profileReducer.userData
+    };
+};
+
+
+if (profTopElem) {
+
+    let ProfileTopConnected = ReactRedux.connect(
+        mapStateToProps, mapDispatchToProps
+    )(ProfileTop);
+
+        ReactDOM.render(
+            <Provider store={store}>
+                <ProfileTopConnected />
+            </Provider>
+            ,
+            profTopElem);
 }
 else if (booksTop) {
     // ReactDOM.render(<BooksTop />, booksTop);
