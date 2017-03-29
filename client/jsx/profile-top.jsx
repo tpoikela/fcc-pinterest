7a8db54e9aaf3c47f401a62ce5011ab1e0659d56
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-29 22:37:40 +0200

    Added call to getUserInfo and rendering of basic user information.

--- client/jsx/profile-top.jsx
+++ client/jsx/profile-top.jsx
@@ -6,23 +6,43 @@ class ProfileTop extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-
-        };
     }
 
     componentDidMount() {
+        this.props.getUserInfo();
+    }
 
+    componentWillReceiveProps() {
+        console.log('componentWillReceiverProps');
     }
 
     render() {
+        console.log('ProfileTop render()');
+        var userData = this.props.userData;
+        var username = '';
+        var userID = '';
+
+        if (userData) {
+            username = userData.username;
+            userID = userData.userID;
+        }
+
         return (
-            <div>App is working!</div>
+            <div>
+                <p>ProfileTop was rendered: |{username}|</p>
+                <p>userID: |{userID}|</p>
+                <button onClick={this.props.onClickButton}>Push</button>
+            </div>
         );
     }
 
 }
 
+ProfileTop.propTypes = {
+    getUserInfo: React.PropTypes.func,
+    onClickButton: React.PropTypes.func,
+    userData: React.PropTypes.object
+
+};
 
 module.exports = ProfileTop;
