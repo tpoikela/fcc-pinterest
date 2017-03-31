aa7ddc78c03848ea10124cf9aa5186295864fa7b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-31 13:58:41 +0200

    Added addLike, addLink, createNew, removeLink, removeLike and corresponding unit tests.

--- server/model/image-schema.js
+++ server/model/image-schema.js
@@ -31,4 +31,66 @@ var ImageSchema = new Schema({
 {collection: 'pint_images'}
 );
 
+ImageSchema.statics.createNew = function(obj, cb) {
+    const Image = this.model('Image');
+    // TODO validate the obj
+    var img = new Image(obj);
+    img.save( (err) => {
+        if (err) {cb(err);}
+        else {
+            cb(null, img);
+        }
+    });
+};
+
+ImageSchema.statics.addLike = function(obj, cb) {
+    const Image = this.model('Image');
+    var id = obj.id;
+    var userId = obj.likedBy;
+    var query = {_id: id};
+    var pushObj = {
+        $push: {likedBy: userId}
+    };
+
+    Image.update(query, pushObj, (err) => {
+        if (err) {cb(err);}
+        else {cb(null);}
+    });
+};
+
+/* Removes one like from the given image.*/
+ImageSchema.statics.removeLike = function(obj, cb) {
+    const Image = this.model('Image');
+    var query = {_id: obj.id};
+    var pullObj = {
+        $pull: {'likedBy.$': obj.userId}
+    };
+
+    Image.update(query, pullObj, (err) => {
+        if (err) {cb(err);}
+        else {cb(null);}
+    });
+};
+
+
+ImageSchema.statics.addLink = function(obj, cb) {
+    const Image = this.model('Image');
+    var id = obj.id;
+    var userId = obj.linkedBy;
+    var query = {_id: id};
+    var pushObj = {
+        $push: {linkedBy: userId}
+    };
+
+    Image.update(query, pushObj, (err) => {
+        if (err) {cb(err);}
+        else {cb(null);}
+    });
+
+};
+
+ImageSchema.statics.removeLink = function(obj, cb) {
+    const Image = this.model('Image');
+};
+
 module.exports = mongoose.model('Image', ImageSchema);
--- test/imageSchemaTests.js
+++ test/imageSchemaTests.js
@@ -6,14 +6,93 @@ const Image = require('../server/model/image-schema.js');
 
 const getObjectId = Utils.getObjectId;
 
+Utils.connectTestDb();
+
+var testUrl = 'http://nonsense.com/test.png';
+
 describe('Image-model', function() {
+
+    var testImg = null;
+
+    beforeEach( (done) => {
+        testImg = new Image({
+            title: 'xxx',
+            url: 'http://yyy.com/img.png',
+            addedBy: getObjectId()
+        });
+        testImg.save( err => {
+            if (err) {throw new Error(err);}
+            done();
+        });
+    });
+
+    afterEach( (done) => {
+        testImg = null;
+        Image.remove({}, err => {
+            if (err) {throw new Error(err);}
+            done();
+        });
+    });
 
     it('should have name, url and owner', function() {
         var img = new Image();
-        img.url = 'http://nonsense.com/test.png';
-        var errors = img.validateSync();
+        img.url = testUrl;
+        var errors = img.validateSync().errors;
+        console.log(JSON.stringify(errors));
         expect(errors.title).to.exist;
         expect(errors.url).to.not.exist;
         expect(errors.addedBy).to.exist;
     });
+
+    it('should be saved properly to DB', function(done) {
+        var obj = {
+            addedBy: getObjectId(),
+            url: testUrl,
+            title: 'A test image for the user'
+        };
+
+        Image.createNew(obj, (err, img) => {
+            expect(err).to.be.null;
+            expect(img._id).to.exist;
+            done();
+        });
+
+    });
+
+    it('can have likes added to it', function(done) {
+        var obj = {id: testImg._id, likedBy: getObjectId()};
+        Image.addLike(obj, (err) => {
+            expect(err).to.be.null;
+            Image.findOne({_id: testImg._id}, (err, img) => {
+                expect(err).to.be.null;
+                if (!img) {
+                    console.log('No image with ID ' + testImg._id);
+                }
+                else {
+                    expect(img.likedBy).to.have.length(1);
+                    done();
+                }
+            });
+
+        });
+    });
+
+    it('can have links added to it', function(done) {
+        var obj = {id: testImg._id, linkedBy: getObjectId()};
+        Image.addLink(obj, (err) => {
+            expect(err).to.be.null;
+            Image.findOne({_id: testImg._id}, (err, img) => {
+                expect(err).to.be.null;
+                if (!img) {
+                    console.log('No image with ID ' + testImg._id);
+                }
+                else {
+                    expect(img.linkedBy).to.have.length(1);
+                    done();
+                }
+            });
+
+        });
+    });
+
 });
