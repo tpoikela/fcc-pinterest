74982f4958900b06070b5de34de6cd471763d792
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-04-03 23:07:34 +0200

    Added masonry. Added like/linkImage.

--- client/jsx/images-top.jsx
+++ client/jsx/images-top.jsx
@@ -4,8 +4,31 @@
 const React = require('react');
 const ImageComp = require('./image');
 
+// const Masonry = require('masonry-layout');
+const Masonry = require('react-masonry-component');
 
+var masonryOptions = {
+    columnWidth: 50
+};
+
+/* Top-level component which shows all images. */
 class ImagesTop extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.likeImage = this.likeImage.bind(this);
+        this.linkImage = this.linkImage.bind(this);
+
+    }
+
+    likeImage(image) {
+        this.props.likeImage(image);
+    }
+
+    linkImage(image) {
+        this.props.linkImage(image);
+    }
 
     componentDidMount() {
         this.props.getAllImages();
@@ -14,20 +37,35 @@ class ImagesTop extends React.Component {
     render() {
         var images = this.props.images;
 
+        // var gridElem = document.querySelector('#image-grid');
+        /* if (gridElem) {
+            console.log('Creating the Masonry fluid grid');
+            this.masonryGrid = new Masonry(gridElem, {
+                columnWidth: 50,
+                itemSelector: '.grid-item'
+            });
+        }*/
+
         var imgElems = images.map( (img, index) => {
+            var likeCb = this.likeImage.bind(this, img);
+            var linkCb = this.linkImage.bind(this, img);
             return (
-                <li key={index}>
-                    <ImageComp image={img}/>
-                </li>
+                <ImageComp image={img} key={index}
+                    likeImage={likeCb}
+                    linkImage={linkCb}
+                />
             );
         });
 
         return (
-            <div className='image-grid'>
+            <div>
                 <h2>ImagesTop</h2>
-                <ul>
-                {imgElems}
-                </ul>
+                <Masonry
+                    elementType={'div'}
+                    options={masonryOptions}
+                    >
+                    {imgElems}
+                </Masonry>
             </div>
         );
 
@@ -37,7 +75,9 @@ class ImagesTop extends React.Component {
 
 ImagesTop.propTypes = {
     getAllImages: React.PropTypes.func,
-    images: React.PropTypes.array
+    images: React.PropTypes.array,
+    likeImage: React.PropTypes.func,
+    linkImage: React.PropTypes.func
 };
 
 module.exports = ImagesTop;
