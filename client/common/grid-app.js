c7c2de6e53ffb8062a85964bb1bf016ef8eae192
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-04-13 05:06:39 +0200

    Added a test for the gallery component.

--- client/common/grid-app.js
+++ client/common/grid-app.js
@@ -1,11 +1,12 @@
 
-
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const Masonry = require('masonry-layout');
+const Gallery = require('../jsx/gallery.jsx');
 
 /*
+const Masonry = require('masonry-layout');
+
 document.onready = () => {
 
     let gridElem = document.querySelector('.image-grid');
@@ -45,16 +46,41 @@ class ImageGrid extends React.Component {
         this.height = 50;
 
         this.onClickAdd = this.onClickAdd.bind(this);
+
+        this.state = {
+            hasMore: true,
+            nElems: 0,
+            elems: []
+        };
     }
 
     onClickAdd() {
         let hPix = this.height + 'px';
-        let style = 'height: ' + hPix;
+        let style = {height: hPix};
+        style.width = '175px';
+        style.backgroundColor = 'red';
+        style.marginTop = '10px';
+        style.marginRight = '10px';
         this.height += 10;
 
         let divElem = document.createElement('div');
         divElem.setAttribute('style', style);
-        this.grid.appendChild(divElem);
+
+        let elemsArray = this.state.elems.slice();
+        elemsArray.push({style: style, height: hPix});
+
+        let nElems = this.state.nElems;
+        let hasMore = true;
+        ++nElems;
+        if (nElems >= 100) {
+            hasMore = false;
+        }
+        this.setState({elems: elemsArray, hasMore: hasMore, nElems: nElems});
+    }
+
+    loadMore() {
+        console.log('image-grid loadMore');
+        this.onClickAdd();
     }
 
     componentDidMount() {
@@ -63,16 +89,14 @@ class ImageGrid extends React.Component {
 
     render() {
 
-        let gridElem = <div className='image-grid' />;
-        this.grid = new Masonry( gridElem, {
-            itemSelector: '.grid-item',
-            columnWidth: 250
-        });
-
         return (
             <div>
-                {gridElem}
-                <button onClick={this.onClickAdd}>Add</button>
+                <div>
+                    <button onClick={this.onClickAdd}>Add</button>
+                </div>
+
+                <Gallery elements={this.state.elems} />
+
             </div>
         );
     }
@@ -85,5 +109,4 @@ ReactDOM.render(
     <ImageGrid />,
     gridComp
 );
-
 
