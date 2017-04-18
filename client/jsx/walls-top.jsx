e96d5d8bf49f624811e3d9b80147dbd1e92e1829
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-04-18 21:15:19 +0200

    Added walls.pug and walls-top.jsx for showing other users walls.

---
+++ client/jsx/walls-top.jsx
@@ -0,0 +1,22 @@
+
+'use strict';
+
+const React = require('react');
+
+class WallsTop extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+            <h2>WallsTop</h2>
+        );
+
+    }
+
+}
+
+module.exports = WallsTop;
