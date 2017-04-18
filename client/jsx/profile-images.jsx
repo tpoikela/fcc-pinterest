d637d27b2d896933401f0ec1499847fe99d75e98
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-04-18 21:21:17 +0200

    Added unlike/unlink callbacks.

--- client/jsx/profile-images.jsx
+++ client/jsx/profile-images.jsx
@@ -17,16 +17,26 @@ class ProfileImages extends React.Component {
 
     render() {
         let images = this.props.images;
+        let likedImages = this.props.likedImages;
+
+        let msg = '';
+        if (images.length === 0) {
+            msg = <p>You haven't added any images.</p>;
+        }
 
         return (
             <div>
+                {msg}
                 <Gallery
                     elements={images}
                     likeImage={this.props.likeImage}
                     linkImage={this.props.linkImage}
+                    unlikeImage={this.props.unlikeImage}
+                    unlinkImage={this.props.unlinkImage}
+                    userLikedImages={likedImages}
+                    userLinkedImages={images}
                 />
             </div>
-
         );
 
     }
@@ -35,9 +45,12 @@ class ProfileImages extends React.Component {
 
 ProfileImages.propTypes = {
     images: React.PropTypes.array,
+    likedImages: React.PropTypes.array,
     likeImage: React.PropTypes.func,
     linkImage: React.PropTypes.func,
-    onClickRemove: React.PropTypes.func
+    onClickRemove: React.PropTypes.func,
+    unlikeImage: React.PropTypes.func,
+    unlinkImage: React.PropTypes.func
 
 };
 
