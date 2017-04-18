3b8edb1b71a37d536b7787c6d0ca793a98ef5c44
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-04-18 21:18:18 +0200

    Added unlink and unlike callbacks.

--- client/jsx/images-top.jsx
+++ client/jsx/images-top.jsx
@@ -2,7 +2,6 @@
 'use strict';
 
 const React = require('react');
-const ImageComp = require('./image');
 const Gallery = require('./gallery');
 
 /* Top-level component which shows all images. */
@@ -13,30 +12,60 @@ class ImagesTop extends React.Component {
 
         this.likeImage = this.likeImage.bind(this);
         this.linkImage = this.linkImage.bind(this);
+        this.unlikeImage = this.unlikeImage.bind(this);
+        this.unlinkImage = this.unlinkImage.bind(this);
 
     }
 
     likeImage(image) {
         this.props.likeImage(image);
+        this.props.getAllImages();
     }
 
     linkImage(image) {
         this.props.linkImage(image);
+        this.props.getAllImages();
+    }
+
+    unlikeImage(image) {
+        this.props.unlikeImage(image);
+        this.props.getAllImages();
+    }
+
+    unlinkImage(image) {
+        this.props.unlinkImage(image);
+        this.props.getAllImages();
     }
 
     componentDidMount() {
+        this.props.getUserInfo();
         this.props.getAllImages();
     }
 
     render() {
         let images = this.props.images;
 
+        let likedImages = [];
+        let linkedImages = [];
+
+        if (this.props.userData) {
+            linkedImages = this.props.userData.linkedTo;
+            likedImages = this.props.userData.liked;
+        }
 
         return (
             <div>
                 <h2>ImagesTop</h2>
 
-                <Gallery elements={images} />
+                <Gallery
+                    elements={images}
+                    likeImage={this.likeImage}
+                    linkImage={this.linkImage}
+                    unlikeImage={this.unlikeImage}
+                    unlinkImage={this.unlinkImage}
+                    userLikedImages={likedImages}
+                    userLinkedImages={linkedImages}
+                />
 
             </div>
 
@@ -48,9 +77,13 @@ class ImagesTop extends React.Component {
 
 ImagesTop.propTypes = {
     getAllImages: React.PropTypes.func,
+    getUserInfo: React.PropTypes.func,
     images: React.PropTypes.array,
     likeImage: React.PropTypes.func,
-    linkImage: React.PropTypes.func
+    linkImage: React.PropTypes.func,
+    unlikeImage: React.PropTypes.func,
+    unlinkImage: React.PropTypes.func,
+    userData: React.PropTypes.object
 };
 
 module.exports = ImagesTop;
