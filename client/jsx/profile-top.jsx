abf6ee1d494e312b0a4702a426f4a084ed193763
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-04-25 23:36:06 +0200

    Added ProfileSearch and new callback addImageFromSearch.

--- client/jsx/profile-top.jsx
+++ client/jsx/profile-top.jsx
@@ -1,15 +1,19 @@
 'use strict';
 
+import {TAB_ADD, TAB_SEARCH} from '../redux/reducers';
+
 const React = require('react');
 
 const ProfileAddImage = require('./profile-add-image');
 const ProfileImages = require('./profile-images');
+const ProfileSearch = require('./profile-search');
 
 class ProfileTop extends React.Component {
 
     constructor(props) {
         super(props);
         this.addImage = this.addImage.bind(this);
+        this.addImageFromSearch = this.addImageFromSearch.bind(this);
     }
 
     componentDidMount() {
@@ -27,48 +31,138 @@ class ProfileTop extends React.Component {
         });
     }
 
+    addImageFromSearch(obj) {
+        obj.userId = this.props.userData._id;
+        this.props.addImage(obj);
+    }
+
     render() {
         console.log('ProfileTop render()');
         let userData = this.props.userData;
         let username = '';
+
+        if (userData) {
+            username = userData.username;
+        }
+
+        let profileTabs = this.getRenderedTabs();
+        let profileBody = this.getRenderedBody(userData);
+
+        return (
+            <div>
+                <p>Welcome {username}!</p>
+                {profileTabs}
+                {profileBody}
+            </div>
+        );
+    }
+
+    /* Returns the tabs that are shown.*/
+    getRenderedTabs() {
+        let changeTab = this.props.changeTab;
+        let shownTab = this.props.shownTab;
+
+        let addClass = shownTab === TAB_ADD ? 'active' : '';
+        let searchClass = shownTab === TAB_SEARCH ? 'active' : '';
+
+        return (
+          <ul className='nav nav-tabs'>
+              <li className={addClass}
+                  onClick={changeTab.bind(this, TAB_ADD)}
+                  >
+                  <a href='#'>Wall</a>
+              </li>
+              <li className={searchClass}
+                  onClick={changeTab.bind(this, TAB_SEARCH)}
+                  >
+                  <a href='#'>Search</a>
+              </li>
+          </ul>
+        );
+    }
+
+    /* Returns the rendered body for profile view based on the status.*/
+    getRenderedBody(userData) {
+        if (this.props.shownTab === TAB_ADD) {
+            return this.getProfileViewAdd(userData);
+        }
+        else if (this.props.shownTab === TAB_SEARCH) {
+            return this.getProfileViewSearch();
+        }
+        else {
+            return <p>An error occurred with tabs!</p>;
+        }
+    }
+
+    /* Returns the view which can be used to search for images.*/
+    getProfileViewSearch() {
+        let images = [];
+
+        if (this.props.searchResults) {
+            images = this.toImages(this.props.searchResults);
+        }
+
+        return (
+            <ProfileSearch
+                addImage={this.addImageFromSearch}
+                images={images}
+                searchImages={this.props.searchImages}
+            />
+        );
+    }
+
+    getProfileViewAdd(userData) {
         let images = [];
         let likedImages = [];
 
         if (userData) {
-            username = userData.username;
             images = userData.linkedTo;
             likedImages = userData.liked;
         }
 
         return (
-            <div>
-                <p>Welcome {username}!</p>
+        <div className='profile-view-add'>
+            <p>
+                You can add images to your profile by using the
+                following fields:
+            </p>
 
-                <p>
-                    You can add images to your profile by using the
-                    following fields:
-                </p>
+            <ProfileAddImage addImage={this.addImage} />
 
-                <ProfileAddImage addImage={this.addImage} />
-
-                <ProfileImages
-                    images={images}
-                    likedImages={likedImages}
-                    likeImage={this.props.likeImage}
-                    linkImage={this.props.linkImage}
-                    onClickRemove={this.props.onClickRemoveImage}
-                    unlikeImage={this.props.unlikeImage}
-                    unlinkImage={this.props.unlinkImage}
-                />
-
-            </div>
+            <ProfileImages
+                images={images}
+                likedImages={likedImages}
+                likeImage={this.props.likeImage}
+                linkImage={this.props.linkImage}
+                onClickRemove={this.props.onClickRemoveImage}
+                unlikeImage={this.props.unlikeImage}
+                unlinkImage={this.props.unlinkImage}
+            />
+        </div>
         );
+
+    }
+
+    /* Converts image search results to an array of Gallery-compatible
+     * objects. */
+    toImages(results) {
+        console.log('toImages results: ' + JSON.stringify(results));
+        let images = results.map( item => {
+            return {
+                url: item.url,
+                title: item.title,
+                linkedBy: [],
+                likedBy: []
+            };
+        });
+        return images;
     }
 
 }
 
 ProfileTop.propTypes = {
     addImage: React.PropTypes.func,
+    changeTab: React.PropTypes.func,
     getUserInfo: React.PropTypes.func,
     likeImage: React.PropTypes.func,
     linkImage: React.PropTypes.func,
@@ -76,6 +170,9 @@ ProfileTop.propTypes = {
     onClickRemoveImage: React.PropTypes.func,
     unlikeImage: React.PropTypes.func,
     unlinkImage: React.PropTypes.func,
+    searchImages: React.PropTypes.func,
+    searchResults: React.PropTypes.array,
+    shownTab: React.PropTypes.string,
     userData: React.PropTypes.object
 };
 
