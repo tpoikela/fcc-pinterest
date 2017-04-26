d7e81818820776c47acdfc9cc670d8d712a85688
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-04-27 00:53:27 +0200

    Removed hardcoded app url. Uses env var APP_URL now.

--- server/config/passport.js
+++ server/config/passport.js
@@ -59,7 +59,7 @@ module.exports = function(passport) {
     passport.use(new TwitterStrategy({
         consumerKey: TWITTER_CONSUMER_KEY,
         consumerSecret: TWITTER_CONSUMER_SECRET,
-        callbackURL: 'http://127.0.0.1:8080/auth/twitter/callback'
+        callbackURL: process.env.APP_URL + ':8080/auth/twitter/callback'
     },
     function(token, tokenSecret, profile, done) {
 		// From https://scotch.io/tutorials/easy-node-authentication-twitter
